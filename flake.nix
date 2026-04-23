a3e8d8eb8f20aa79e7bb91a54abf94f1519c7b5c
Lukas Mertens <git@lukas-mertens.de>
2026-04-23 19:46:26 +0000

    chore: nix dev shell

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Python development environment with aiohttp";
+
+  inputs = {
+    # Using the stable nixpkgs branch
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems to support
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for each system
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          
+          # Define your Python environment
+          pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+            aiohttp
+            # You can add more packages here, e.g.,
+            # requests
+            # pytest
+          ]);
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pythonEnv
+            ];
+
+            # Optional: Shell commands to run upon entering the environment
+            shellHook = ''
+              echo "🐍 Python dev shell"
+              python --version
+            '';
+          };
+        });
+    };
+}
